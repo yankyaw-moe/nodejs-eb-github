337ea3214bebcb9c4cc4d772f5fed11ed5e7edde
Yan Kyaw Moe <yankyawmoe.dev@gmail.com>
2022-05-31 23:21:21 +0630

    update: test ci/cd

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 app.get('/', (req, res) => {
-    res.send('Hello world Testing Node EB Github!')
+    res.send('Hello world Testing Node EB Github Update CI/CD!')
 })
 
 
