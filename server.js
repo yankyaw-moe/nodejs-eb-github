17f267110edc4b47b2622a2e1d829b2fcd4306cd
Yan Kyaw Moe <yankyawmoe.dev@gmail.com>
2022-05-31 23:05:22 +0630

    init

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+app.get('/', (req, res) => {
+    res.send('Hello world Testing Node EB Github!')
+})
+
+
+let port = process.env.PORT || 3000;
+console.log('port ', port)
+console.log('env port ', process.env.PORT);
+
+
+app.listen(port, () => {
+    console.log(`Server is up on ${port}`)
+})
